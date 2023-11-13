fb237a5f514787ad24875df1964030611e6f8466
G00422756@atu.ie <G00422756@atu.ie>
2023-11-13 17:05:59 +0000

    commited TryCatch2

--- src/main/java/ie/atu/TryCatch.java
+++ src/main/java/ie/atu/TryCatch.java
@@ -1,21 +1,32 @@
 package ie.atu;
+
 import java.util.Scanner;
+
 public class TryCatch {
 
     public static void main(String[] args) {
-    System.out.println("Please enter a number between 0 - 9: ");
-    Scanner myscan = new Scanner(System.in);
 
-    String input = myscan.nextLine();
+        while(true) {
+            System.out.println("Please enter a number between 0 - 9: ");
+            Scanner myscan = new Scanner(System.in);
+            try {
+                String input = myscan.nextLine();
 
-    int myNum = Integer.parseInt(input);
+                int myNum = Integer.parseInt(input);
 
-    if(myNum >= 0 && myNum <= 9) {
-        System.out.println("You entered " + myNum);
+                if (myNum >= 0 && myNum <= 9) {
+
+                    System.out.println("You entered " + myNum);
+                    break;
+
+                } else{
+                    System.out.println("Not a valid number");
+
+                }
+
+            } catch (NumberFormatException nfe) {
+                System.out.println("Not a valid number");
+            }
+        }
     }
-    else{
-        System.out.println("Not a valid number");
-    }
-}
-
 }
